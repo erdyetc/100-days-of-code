84ef68a545bd469941ac589af6338af9b1c13fbf
Edwina Liu <edwinaliu@Edwinas-Air.attlocal.net>
2023-09-15 14:35:29 -0700

    git instructions

---
+++ Day 24/main.py
@@ -0,0 +1,8 @@
+#Github instructions (once in folder in terminal, nav with cd)
+#Use "folder name" in quotes when mutliple words
+
+# git add . 
+
+# git commit -m "{message}"
+
+# git push origin main
